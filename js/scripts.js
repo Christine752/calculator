c09cfa4076b58ed2f2cc28efeb4db987addb8a81
Christine752 <njeri.mugamby@gmail.com>
2020-03-26 16:40:48 +0300

    add changes to scripts.js file in js folder

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-var bodyMassIndex = function(weight, height) {
-  return weight / height;
+var add = function(number1, number2) {
+  return number1 + number2;
 };
 
-var height = parseInt(prompt("Enter your weight in kilograms:"));
-var weight = parseFloat(prompt("Enter your height in meters squared:"));
-var result = bodyMassIndex(weight, height);
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
 alert(result);
