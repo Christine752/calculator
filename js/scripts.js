ae0892c4c1fd90db33149dffc88075606a632970
Christine752 <njeri.mugamby@gmail.com>
2020-04-01 23:18:39 +0300

    add changes to scripts.js file in js folder

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,10 @@ var divide = function(number1, number2) {
 };
 
 $(document).ready(function() {
-  var number1 = parseInt($("#add1").val());
-  var number2 = parseInt($("#add2").val());
-  alert(add(number1, number2));
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    alert(add(number1, number2));
+  });
 });
