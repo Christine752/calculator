c8e73ec21399a7d907f32ef671e0f87ad4713755
Christine752 <njeri.mugamby@gmail.com>
2020-04-01 23:01:09 +0300

    add changes to scripts.js filein js folder

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,6 @@ $(document).ready(function() {
   var number1 = parseInt(prompt("Enter a number:"));
   var number2 = parseInt(prompt("Enter another number:"));
   alert(add(number1, number2));
+  $("#add1").val();
+  $("#add2").val();
 });
